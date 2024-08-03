a30f1a80830b677b8aba230ddd5aa8e8254dc0b1
Ruth Meissner <ruthmeissner96@gmail.com>
2024-08-03 10:11:17 +0200

    added dev and build scripts

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['index.html', './src/**/*.{html,js}'], // Include any files that you want to apply the styles to
+    theme: {
+      extend: {}
+    },
+    plugins: []
+  };
