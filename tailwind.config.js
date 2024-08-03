f202ecb1af339a05610622f3a05d47292408a021
Ruth Meissner <ruthmeissner96@gmail.com>
2024-08-03 12:30:11 +0200

    refactored main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['index.html', './src/**/*.{html,js}'], // Include any files that you want to apply the styles to
+    content: ['index.html', 'main.js', 'journal.js',"journal.html", './src/**/*.{html,js}'], // Include any files that you want to apply the styles to
     theme: {
       extend: {}
     },
